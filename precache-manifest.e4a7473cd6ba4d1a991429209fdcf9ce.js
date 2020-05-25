f024f291d3c020e84571e15d8035011455142c9f
José David Melián Villalobos <josedavidmelian@gmail.com>
2020-05-25 01:09:58 +0100

    Updates

---
+++ precache-manifest.e4a7473cd6ba4d1a991429209fdcf9ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f0aedaba58c931b357a0599cdeea3c0",
+    "url": "/simple-react-project/index.html"
+  },
+  {
+    "revision": "2715c9298d63dbe04dcf",
+    "url": "/simple-react-project/static/css/main.1ddd0cb4.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/simple-react-project/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-project/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2715c9298d63dbe04dcf",
+    "url": "/simple-react-project/static/js/main.c70c209a.chunk.js"
+  },
+  {
+    "revision": "43cdaa15f45442e9da98",
+    "url": "/simple-react-project/static/js/runtime-main.7ef3099a.js"
+  }
+]);
